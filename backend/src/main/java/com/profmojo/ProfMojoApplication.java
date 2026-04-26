de5f8d3807fd189bada481a9037bb52529b2552f
RISHABHXVAZ <srivastavarishabh799@gmail.com>
2026-04-27 03:25:14 +0530

    -

--- backend/src/main/java/com/profmojo/ProfMojoApplication.java
+++ backend/src/main/java/com/profmojo/ProfMojoApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class
 ProfMojoApplication {
 
 	public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(ProfMojoApplication.class, args);
 	}
 }
